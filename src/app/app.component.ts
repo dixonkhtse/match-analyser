a05bcde722b76a6a613b89b4e7fff154f9f65ef6
Dixon <dixon@nanoinsure.com>
2021-06-03 18:27:01 +0800

    initial UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { split, find } from 'lodash';
+import { Promise } from 'bluebird';
+import { Constants } from './common/constants';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'match-analyser';
+  public matchUrl = '';
+  public faceitUsername = '';
+  public submitted = false;
+  public analysing = false;
+  public result: any;
+  public warning = '';
+
+  constructor(private http: HttpClient) { }
+
+  async analyse() {
+    this.warning = '';
+    this.submitted = true;
+    this.analysing = true;
+    try {
+      const options: any = {
+        headers: { Authorization: `Bearer ${Constants.AUTH_TOKEN}` },
+      };
+      const splitted = split(this.matchUrl, '/');
+      const matchIdIdx = splitted.indexOf('room') + 1;
+      const matchId = splitted[matchIdIdx];
+      if (matchIdIdx === 0 || !matchId) {
+        throw new Error('match_id not found.');
+      }
+      const response: any = await this.http.get(`${Constants.GET_MATCH_ENDPOINT}/${matchId}`, options).toPromise();
+      if (!response) {
+        throw new Error('Result not found.');
+      }
+      const { teams } = response;
+      let targetFaction = find(teams, ({ roster }) => find(roster, ({ nickname }) => nickname === this.faceitUsername));
+      if (!targetFaction) {
+        ({ faction1: targetFaction } = teams);
+      }
+      const rows = [];
+      await Promise.mapSeries(targetFaction.roster, ({ nickname, game_skill_level, player_id }) => {
+        rows.push({ nickname, game_skill_level, player_id });
+      });
+      this.result = rows;
+    } catch (ex) {
+      this.warning = ex.message;
+      this.result = null;
+    } finally {
+      this.analysing = false;
+    }
+  }
+
+  reset() {
+    this.warning = '';
+    this.submitted = false;
+    this.matchUrl = '';
+    this.faceitUsername = '';
+  }
 }
---
+++ src/app/common/constants.ts
@@ -0,0 +1,4 @@
+export class Constants {
+  public static AUTH_TOKEN = '49f830fc-93cc-4dae-8006-01b1d7e0b8dd';
+  public static GET_MATCH_ENDPOINT = 'https://open.faceit.com/data/v4/matches';
+}
